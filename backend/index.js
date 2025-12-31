fbc2d54667dcad9c22dc6bf9bd3807a320ec5ae9
josephlesterbacs <106429121+jlbbacs@users.noreply.github.com>
2026-01-01 01:48:33 +0800

    fixfdfdf

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express(); // Define app first
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:5000', 'https://book-mern-stack.vercel.app'], 
+  origin: ['http://localhost:5173', 'https://book-mern-stack.vercel.app'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
